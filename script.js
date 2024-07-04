7e8845fed2448f1034243e9e2f35162711cd0395
Slyor <blafil0703@gmail.com>
2024-07-04 11:02:30 +0200

    improved code

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 //define initial values
-let userChoice
-let computerChoice
-let result
+let userChoice;
+let computerChoice;
+let result;
+let userScore = 0;
+let computerScore = 0;
 
 //returns a random number between 0 and max value as a float, then rounds it to the nearest integer
 function getRandomInt(max){
   return Math.floor(Math.random() * max)
 }
+/*
+Gets a random number between 0 and 3. Not inclusive, because 3 is not included. Numbers can be 0,1 or 2.
+Defines an array "rps" with 3 values.
+The computer choice is then converted from the random integer to a key value in the array "rps"
+*/
+function getComputerChoice(){
+  computerChoice = getRandomInt(3);
+  rps = ["rock", "paper", "scissors"];
+  computerChoice = rps[computerChoice];
+}
 
 
 //These 3 functions define the value of userChoice, depending on which one he clicks in the HTML
+
 function rock(){
   userChoice = "rock";
 }
@@ -20,36 +33,29 @@ function scissors(){
   userChoice = "scissors"
 }
 
-/*
-Gets a random number between 0 and 3. Not inclusive, because 3 is not included. Numbers can be 0,1 or 2.
-Defines an array "rps" with 3 values.
-The computer choice is then converted from the random integer to a key value in the array "rps"
-*/
-function computerChoose(){
-  computerChoice = getRandomInt(3);
-  rps = ["rock", "paper", "scissors"];
-  computerChoice = rps[computerChoice];
-}
-
 
 //show the result of the game
-function showResult(){
+function calculateScore(){
   if (userChoice == computerChoice){
-    result = "Draw!"
+    return
   } else if (userChoice == "rock" && computerChoice == "scissors"
     || userChoice == "paper" && computerChoice == "rock"
     || userChoice == "scissors" && computerChoice == "paper") {
-    result = "You win!"
+    return userScore = ++userScore
   } else{
-    result = "You lose!"
-  } 
-  alert(`${result.toUpperCase()} \n\nYou chose ${userChoice}, computer chose ${computerChoice}!`)
+    return computerScore = ++computerScore
+  }
 }
 
-function userClick(action1,action2,action3){
+function showScore(){
+  return alert(`Your score is ${userScore}, pc score is ${computerScore}`);
+}
+
+function userClick(action1,action2,action3,action4){
   action1()
   action2()
   action3()
+  action4()
 }
 
 /*
